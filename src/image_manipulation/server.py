4a873e0bb58cf5d930eed2eb77f337718c91fcdd
Saurabh Nair <saurabh@vicarious.com>
2021-08-12 12:52:37 -0700

    basic server code

--- src/image_manipulation/server.py
+++ src/image_manipulation/server.py
@@ -1,8 +1,19 @@
+from image_manipulation.image_pb2 import  NLImageRotateRequest, NLImageService
 from fire import Fire
+from concurrent import futures
+from grpc import server
 
-def print_hello(msg):
-    print(f"Success: {msg}")
+
+def run_service_request():
+    server = server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+
 
 
 def main():
-    Fire(print_hello)
+    Fire(run_service_request)
