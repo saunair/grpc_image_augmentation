6362ba28a4b47e92b6f8696512e3ff21ad23c7ef
Saurabh Nair <saurabh@vicarious.com>
2021-08-12 23:35:42 -0700

    communication tests

---
+++ tests/test_communication.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+from mock import Mock
+from copy import copy
+
+from image_manipulation.communication_utils import ImageService
+from image_manipulation import image_utils
+
+
+def test_service_object():
+    service_object = ImageService()
+    # Test an expected case.
+    input_image_path = "testing_data/image.jpg" 
+    input_image = cv2.imread(input_image_path)
+    valid_image_pb = image_utils.convert_image_to_proto(input_image)
+
+    # Just mocking the context for now as we don't care about where it sends the output to.
+    op_pb = service_object.MeanFilter(valid_image_pb, context=Mock())
+    assert op_pb.width == 1080
+    
+    invalid_pb_image = copy(valid_image_pb)
+    invalid_pb_image.width = 80
+    op_pb = service_object.MeanFilter(invalid_pb_image, context=Mock())
+    assert op_pb.width == 0
